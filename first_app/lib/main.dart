b9d6de6c11a7ad01662329c661c19f562e4c2098
Yahya Ahmer <yahyaahmer62@gmail.com>
2025-04-29 16:07:14 +0500

    style: wrap the Column with Center

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -41,18 +41,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;                 // ← Add this.
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),                // ← Change to this.
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random AWESOME idea:'),
+            BigCard(pair: pair),                // ← Change to this.
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
