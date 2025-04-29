2b0a2a972f97472724148868aed91376ce5195f6
Yahya Ahmer <yahyaahmer62@gmail.com>
2025-04-29 20:25:30 +0500

    feat: Add the button section to the children list

--- flutter_layout/lib/main.dart
+++ flutter_layout/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
                 name: 'Oeschinen Lake Campground',
                 location: 'Kandersteg, Switzerland',
               ),
+              ButtonSection(),
             ],
           ),
         ),
